a4c1b440252c7c1c4a32896dab407bf44eaa99a6
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-18 14:56:53 -0700

    menu nav animation

--- src/js/mobileNav.js
+++ src/js/mobileNav.js
@@ -3,9 +3,13 @@ const closeBtn = $('.close-btn');
 const menuOverlay = $('.menu-container');
 
 menuBtn.click(() => {
-    menuOverlay.css('display', 'flex');
+    menuOverlay.css('opacity', '100');
+    menuOverlay.css('pointer-events', 'all');
+    menuOverlay.css('transform', 'translateY(0px)');
 });
 
 closeBtn.click(() => {
-    menuOverlay.css('display', 'none');
+    menuOverlay.css('opacity', '0');
+    menuOverlay.css('pointer-events', 'none');
+    menuOverlay.css('transform', 'translateY(200px)');
 });
