b161b441ae52d596c3797434c056a3eba0621d54
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-18 22:54:17 -0700

    attempt to prevent animation from triggering on resize - minor visual adjustments

--- src/js/mobileNav.js
+++ src/js/mobileNav.js
@@ -1,15 +1,22 @@
-const menuBtn = $('.menu-btn');
-const closeBtn = $('.close-btn');
-const menuOverlay = $('.menu-container');
+const menuBtn = $(".menu-btn");
+const closeBtn = $(".close-btn");
+const menuOverlay = $(".menu-container");
+
+$("window").resize(function () {
+  $("body").add(".resize-animation-handler");
+  setTimeout(function () {
+    $("body").remove(".stop-transitions");
+  }, 400);
+});
 
 menuBtn.click(() => {
-    menuOverlay.css('opacity', '100');
-    menuOverlay.css('pointer-events', 'all');
-    menuOverlay.css('transform', 'translateY(0px)');
+  menuOverlay.css("opacity", "100");
+  menuOverlay.css("pointer-events", "all");
+  menuOverlay.css("transform", "translateY(0px)");
 });
 
 closeBtn.click(() => {
-    menuOverlay.css('opacity', '0');
-    menuOverlay.css('pointer-events', 'none');
-    menuOverlay.css('transform', 'translateY(200px)');
+  menuOverlay.css("opacity", "0");
+  menuOverlay.css("pointer-events", "none");
+  menuOverlay.css("transform", "translateY(200px)");
 });
