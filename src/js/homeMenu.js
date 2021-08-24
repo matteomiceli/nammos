c4242da485d00289ca38f841495b3f982521e214
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-24 16:12:00 -0700

    menu integrated

--- src/js/homeMenu.js
+++ src/js/homeMenu.js
@@ -1,3 +1,17 @@
-const toggleExpand = $('#arrow');
-const menuContainer = $('#sp_iframe');
+const toggleExpand = $('#toggle-menu-btn');
+const arrow = $('#arrow');
+const menuContainer = $('#menusContainer');
+const sectionText = $('#s1  h2');
 
+toggleExpand.on('click', (e) => {
+    e.preventDefault();
+    if (!menuContainer.hasClass('expanded')) {
+        menuContainer.addClass('expanded');
+        arrow.css('transform', 'rotate(180deg)');
+        sectionText.css('margin-bottom', '40px');
+    } else {
+        menuContainer.removeClass('expanded');
+        arrow.css('transform', 'rotate(0deg)')
+        sectionText.css('margin-bottom', '0px')
+    }
+})
