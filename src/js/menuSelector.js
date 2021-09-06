b76e729ae2df60d1d0b4ac76aa0cd1236fa6ec1d
Matteo Miceli <omatteomiceli@gmail.com>
2021-09-06 00:57:12 -0700

    heights entered in menuList object - tbd: adjust height based on which menu is selected

--- src/js/menuSelector.js
+++ src/js/menuSelector.js
@@ -12,9 +12,18 @@ const url = "https://menus.singleplatform.com/widget"
 
 // list of 
 const listOfMenuUrls = {
-  'brunch': 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=3070970&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
-  'dinner': 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2090069&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
-  'drinks': 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2946394&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+  'brunch': {
+    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=3070970&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    height: '1564',
+  },
+  'dinner': {
+    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2090069&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    height: '3598',
+  },
+  'drinks': {
+    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2946394&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    height: '4521'
+  },
   'dessert': ''
 }
 
@@ -29,11 +38,8 @@ function switchMenu (eventTarget, url) {
   selector.addClass('menu-selector-active');
   console.log(selector)
 
-  // // change menu iframe menu id
+  // change menu iframe menu id and reload iFrame
   menuIframe.attr('src', url)
-
-  // // // reload iframe
-  // menuIframe.attr( 'src', function ( i, val ) { return val; });
 }
 
 
@@ -48,5 +54,5 @@ console.log(selectors)
   selectors.removeClass('menu-selector-active');
   
   // call switch function
-  switchMenu(eventTarget, listOfMenuUrls[eventTarget.id])
+  switchMenu(eventTarget, listOfMenuUrls[eventTarget.id].src)
 })
