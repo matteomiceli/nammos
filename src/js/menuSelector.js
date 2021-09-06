9ba7fb70564518992d47ca5c4a29f46fa86524af
Matteo Miceli <omatteomiceli@gmail.com>
2021-09-06 11:49:07 -0700

    added https to list of urls

--- src/js/menuSelector.js
+++ src/js/menuSelector.js
@@ -13,15 +13,15 @@ const url = "https://menus.singleplatform.com/widget"
 // list of 
 const listOfMenuUrls = {
   'brunch': {
-    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=3070970&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    src: 'https://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=3070970&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
     height: '1564',
   },
   'dinner': {
-    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2090069&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    src: 'https://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2090069&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
     height: '3598',
   },
   'drinks': {
-    src: 'http://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2946394&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
+    src: 'https://places.singleplatform.com/nammos-estiatorio-0/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=16px&display_menu=2946394&hide_cover_photo=true&hide_disclaimer=true&menu_desc_background_color=%23ffffff&primary_background_color=%23ffffff&section_desc_background_color=%23ffffff&section_title_background_color=%23ffffff&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29',
     height: '4521'
   },
   'dessert': ''
